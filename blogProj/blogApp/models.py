521fc1468ec123885c74c3d27c9cea513d8736ce
sherif elshafeay <sherifelshaf3y@yahoo.com>
2022-02-10 20:02:01 +0200

    intilize project

---
+++ blogProj/blogApp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    followers = models.ManyToManyField(User)
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+class Comment(models.Model):
+    body = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User)
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    postpicture = models.ImageField(upload_to='images/')
+    content = models.CharField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User)
