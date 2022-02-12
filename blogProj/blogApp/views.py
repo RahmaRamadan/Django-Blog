12020f82d2d2bda8a020811f7d33e5b6a06c8162
sherif elshafeay <sherifelshaf3y@yahoo.com>
2022-02-12 20:14:18 +0200

    blogAppUrls

--- blogProj/blogApp/views.py
+++ blogProj/blogApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+# from .models import Student, Track
+# from .forms import StudentForm, UserForm
+
+
 
 # Create your views here.
+def home(request):
+    return render(request,'blogApp/home.html')
