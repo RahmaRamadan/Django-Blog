cd6da220afb670f73d3e0e098d072414e6df9b02
RahmaRamadan <rahmaramadan23@gmailcom>
2022-02-13 15:33:32 +0200

    remodify admin

--- blogProj/blogApp/admin.py
+++ blogProj/blogApp/admin.py
@@ -11,31 +11,3 @@ class UserAdmin(admin.ModelAdmin):
     search_fields=["username","password"]
 admin.site.register(User,UserAdmin)
 
-
-
-
-
-
-
-
-
-
-
-# class StudentAdmin(admin.ModelAdmin):
-#     fieldsets=(
-#     ["student Information",{'fields':["fname","lname","age"]}],
-#     ["Scholarship Information",{'fields':["student_track"]}]
-#     )
-#     list_display=("fname","lname","age","student_track","is_graduate")
-#     list_filter=["fname","age","student_track"]
-#     search_fields=["fname","age","student_track__track_name"]
-# class InlineStudent(admin.StackedInline):
-#     model=Student
-#     extra =2
-# class TrackAdmin(admin.ModelAdmin):
-#     inlines=[InlineStudent]
-
-    
-# admin.site.register(Student,StudentAdmin)
-# admin.site.register(Track,TrackAdmin)
-
