65e2d5ee765b8c10d63617f122449905f79a4914
shrouq hussein <shrouq.hussein.elsayed@gmail.com>
2022-02-13 03:27:45 -0800

    adding admin

--- blogProj/blogApp/models.py
+++ blogProj/blogApp/models.py
@@ -11,9 +11,13 @@ class User(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50)
     followers = models.ManyToManyField(User)
+    def __str__(self):
+        return self.name 
     
 class Tag(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
     
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -22,11 +26,15 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # date = models.DateField()
+    def __str__(self):
+        return self.title + '|' + str(self.user)
     
 class Comment(models.Model):
     body = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 # student_track = models.ForeignKey(Tracks, on_delete=models.CASCADE)
 
