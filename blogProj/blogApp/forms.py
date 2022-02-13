d1b4b09c221cefda937833feb7037259b55a3e3b
shrouq hussein <shrouq.hussein.elsayed@gmail.com>
2022-02-13 06:11:19 -0800

    adding post,delete

--- blogProj/blogApp/forms.py
+++ blogProj/blogApp/forms.py
@@ -1,9 +1,11 @@
 from dataclasses import fields
 from re import L
 from django import forms
+from .models import Post
 #auth import
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 
 
 class UserForm(UserCreationForm):
@@ -11,3 +13,9 @@ class UserForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
         # fields = ('__all__')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','content','category','user','tags')
--- blogProj/blogApp/urls.py
+++ blogProj/blogApp/urls.py
@@ -4,9 +4,16 @@ from . import views
 urlpatterns = [
 
     path('home',views.home, name='home'),
+    path('post',views.post, name='post'),
+    path('postDetails/<post_id>',views.postDetails , name='postDetails'),
+    path('addPost',views.addPost , name='addPost'),
+    path('deletePost/<post_id>',views.deletePost, name='deletePost'),
+    # path('editPost/<post_id>',views.editPost , name='editPost'),
+    
     #auth urls
     path('login',views.loginPg , name='login'),
     path('signup',views.signupPg , name='signup'),
     path('signout',views.signoutPg , name='signout'),
+     
 
 ]
