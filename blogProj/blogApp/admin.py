4b658d77c67c1f3493d27d98bf9f411ccafe36aa
RahmaRamadan <rahmaramadan23@gmailcom>
2022-02-14 08:38:15 +0200

    modify admin features

--- blogProj/blogApp/admin.py
+++ blogProj/blogApp/admin.py
@@ -1,13 +1,54 @@
 from dataclasses import field
 from django.contrib import admin
-from .models import User
-# # Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    fieldsets = (
-        ["User Info", {'fields':["username","email","password"]}],
-    )
-    list_display=("username","email","password")
-    list_filter=["username","email"]
-    search_fields=["username","password"]
-admin.site.register(User,UserAdmin)
+from .models import User, Category, UsersCategories, Post, Comment, Tag
 
+
+class UserCategoryInline(admin.TabularInline):
+    model = UsersCategories
+    extra = 1
+
+
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "username", "email", "password")
+    list_filter = ["username", "email"]
+    search_fields = ["username"]
+    inlines = (UserCategoryInline,)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'show_followers')
+    list_filter = ["name"]
+    search_fields = ["name"]
+    inlines = (UserCategoryInline,)
+
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ["post info", {'fields': ["title", "postpicture",
+                                  "content", "category", "tags", "user"]}],
+    )
+    # list_display=("title","postpicture","content","category","tags","user","date")
+    search_fields = ["user"]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ["comment info", {'fields': ["body", "post", "user"]}],
+    )
+    list_display = ("body", "post", "user")
+    search_fields = ["post", "user"]
+
+
+class TagAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ["tag info", {'fields': ["name"]}],
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Tag, TagAdmin)
--- blogProj/blogApp/models.py
+++ blogProj/blogApp/models.py
@@ -1,32 +1,66 @@
+from tkinter import CASCADE
+from django import forms
 from django.db import models
-
+from datetime import datetime, date
 # Create your models here.
 
+
 class User(models.Model):
+    class Meta:
+        ordering = ['pk']
     username = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
     def __str__(self):
         return self.username
+
+
 class Category(models.Model):
+    class Meta:
+        ordering = ['pk']
     name = models.CharField(max_length=50)
-    followers = models.ManyToManyField(User)
+    followers = models.ManyToManyField(User, through='UsersCategories')
     
+    def __str__(self):
+        return self.name
+
+    def show_followers(self):
+        return "\n".join([a.username for a in self.followers.all()])
+    
+class UsersCategories(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
 class Tag(models.Model):
+    class Meta:
+        ordering = ['pk']
     name = models.CharField(max_length=50)
-    
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
+    class Meta:
+        ordering = ['pk']
     title = models.CharField(max_length=50)
-    postpicture = models.ImageField(upload_to='images/')
+    postpicture = models.ImageField(upload_to='blogApp/static/img/')
     content = models.CharField(max_length=300)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    post_date = models.DateField(auto_now_add=True)
+    # date = models.DateField()
+
+    def __str__(self):
+        return self.title + '|' + str(self.user)
+
+
 class Comment(models.Model):
+    class Meta:
+        ordering = ['pk']
     body = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-# student_track = models.ForeignKey(Tracks, on_delete=models.CASCADE)
-
