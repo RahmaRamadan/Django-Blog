e143a7eaf75b1abf2726617135dc7ae170827091
NehalHassan <nehalhdev@gmail.com>
2022-02-18 00:01:53 +0200

    adding-styles

--- blogProj/blogApp/forms.py
+++ blogProj/blogApp/forms.py
@@ -49,12 +49,11 @@ class PostForm(forms.ModelForm):
 
         fields = ('title','content','category','tags','postpicture')
         widgets = {
-            # 'title': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'content': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'category':forms.Select(attrs={'class': 'form-control'}),
-            # 'user' : 
-            # 'tags':forms.Select(attrs={'class': 'form-control'}),
-            # 'postpicture':forms.ClearableFileInput(attrs={'class': 'form-control'})
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'category':forms.Select(attrs={'class': 'form-control'}), 
+            'tags':forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'postpicture':forms.FileInput(attrs={'class': 'form-control'})
         }
 
 class CommentForm(forms.ModelForm):
